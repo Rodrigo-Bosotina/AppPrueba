82f75ae7ba983a3b7268424ad88f7feab677d9fb
Rodrigo Bosotina <bosotinarodrigo@gmail.com>
2023-09-05 18:47:44 -0300

    App de la Verduleria

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
+
   production: false,
   firebase: {
-    apiKey: 'xxx',
-    authDomain: 'bezkoder-firebase.firebaseapp.com',
-    databaseURL: 'https://bezkoder-firebase.firebaseio.com',
-    projectId: 'bezkoder-firebase',
-    storageBucket: 'bezkoder-firebase.appspot.com',
-    messagingSenderId: 'xxx',
-    appId: 'xxx'
+    apiKey: "AIzaSyDuJ11dz-uX0P-IYEEzC7uhCwY6yIraw54",
+    authDomain: "training-it-9ead4.firebaseapp.com",
+    databaseURL: 'https://console.firebase.google.com/project/undefined/firestore/data/',
+    projectId: "training-it-9ead4",
+    storageBucket: "training-it-9ead4.appspot.com",
+    messagingSenderId: "1005321975028",
+    appId: "1:1005321975028:web:8933f51908006f2dd15aa1"
   }
 };
